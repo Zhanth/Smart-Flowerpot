d4377254fa6f9c35109975a0faabfcb56f8ff4c7
Zhanth <jorgeplehmann@gmail.com>
2018-03-03 11:00:12 +0000

    Raindrop sensor code added

---
+++ Raindrop Sensor.py
@@ -0,0 +1,16 @@
+#link: http://raspi.tv/2017/make-a-rain-alert-system-with-raspberry-pi   
+#
+# raindrop sensor DO connected to GPIOx
+# HIGH = no rain, LOW = rain detected
+#import RPi.GPIO as GPIO
+#
+import time
+from gpiozero import InputDevice
+ 
+no_rain = InputDevice(x)
+ 
+while True:
+    if not no_rain.is_active:
+        print("Rain detected!")
+        # other code or functions
+    time.sleep(1) 
