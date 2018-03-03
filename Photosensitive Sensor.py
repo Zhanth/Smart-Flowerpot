ebbd9a583addadfc92803edce8f69466ad54f7de
Zhanth <jorgeplehmann@gmail.com>
2018-03-03 14:15:17 +0000

    Update

--- Photosensitive Sensor.py
+++ Photosensitive Sensor.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
- 
+import time
+
 channel = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel,GPIO.IN)
- 
-for i in range(0,5):
-    print GPIO.input(channel)
+
+try:
+	if GPIO.input(channel):
+    	print ("Light detected.")
+    	#Log sunlight time
+    else:
+    	print ("No light detected.")
+
+while True:
+        time.sleep(1)
