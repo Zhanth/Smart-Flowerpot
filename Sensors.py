d7c392d499590f297a6d7fcb74b88aa200096f42
Zhanth <Zh4nth@gmail.com>
2018-03-12 09:13:10 +0000

    Time events update

--- Sensors.py
+++ Sensors.py
@@ -13,13 +13,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(soil, GPIO.IN)
 GPIO.setup(rain, GPIO.IN)
 GPIO.setup(light,GPIO.IN)
+active_time = time.asctime()
  
 def callback(soil):
         if GPIO.input(soil):
                 print "Your plants are ok :)"
         else:
-                print "Time to water the plants!"
-                # Activate "must water" icon on app
+                print "Time to water the plants!" #Activate "must water" icon
 
 GPIO.add_event_detect(soil, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(soil, callback)
@@ -27,22 +27,18 @@ GPIO.add_event_callback(soil, callback)
 try: 
 
 	if GPIO.input(rain):
-		active_time = time.time()
-		print ("Rain detected!")
-		print (active_time)
-		# other code or functions
-        # Log rainy days
-        # Activate rainy icon on app
-        # Deactivate "must water" icon on app
+		print ("Rain detected!") #Activate rainy icon | Deactivate "must water" icon
+		print(tiempo + " It's raining.") #Log
     else:
-    	print ("No rain detected.")
-    	print (active_time)
+    	print ("No rain detected.") #Deactivate rainy icon
+    	print(tiempo + " It's not raining.") #Log
 
     if GPIO.input(light):
     	print ("Light detected.")
-    	#Log sunlight time
+        print(tiempo + " It's sunny") #Log
     else:
-    	print ("No light detected.")	
+    	print ("No light detected.")
+        print(tiempo + " It's shady") #Log	
 
     if humidity is not None and temperature is not None:
  		print('Temperatura={0:0.1f}*C  Humedad={1:0.1f}%'.format(temperature, humidity))
